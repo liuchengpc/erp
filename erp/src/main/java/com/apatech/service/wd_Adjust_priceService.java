95d79ed9b4f56f833d59ce0a05852e601c152ea4
HuNao0102 <3078129195@qq.com>
2020-02-08 13:19:03 +0800

    202002081318wd

--- erp/src/main/java/com/apatech/service/wd_Adjust_priceService.java
+++ erp/src/main/java/com/apatech/service/wd_Adjust_priceService.java
@@ -3,6 +3,7 @@ package com.apatech.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.apatech.domain.Adjust_price;
 import com.apatech.domain.wd_Adjust_price;
@@ -10,6 +11,7 @@ import com.apatech.mapper.Adjust_priceMapper;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
+@Service
 public class wd_Adjust_priceService {
 	@Autowired
 	private Adjust_priceMapper dao;
@@ -19,9 +21,17 @@ public class wd_Adjust_priceService {
 		 
     	PageHelper.startPage(pageNum, pageSize);
     	List<wd_Adjust_price> list=dao.wdselectAll();
-
     	PageInfo<wd_Adjust_price> page=new PageInfo<wd_Adjust_price>(list);
 //	    	System.out.println("分页的集合2："+page.getList().toString());
     	return page;
     }
+	 
+	 public List<wd_Adjust_price> queryMater(String matterCustom6){
+	    
+	    	List<wd_Adjust_price> list=dao.queryMater(matterCustom6);
+	    	
+	    	return list;
+	    }
+	 
+	 
 }

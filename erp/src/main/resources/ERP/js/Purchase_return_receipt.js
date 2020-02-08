ae03407f0ed339b3e22426605a2a6a54911ef4b9
shaw <429312645@qq.com>
2020-02-08 19:26:25 +0800

    添加弹框脚本

--- erp/src/main/resources/ERP/js/Purchase_return_receipt.js
+++ erp/src/main/resources/ERP/js/Purchase_return_receipt.js
@@ -1,4 +1,7 @@
-var orderStatusMeta = {
+layui.use('layer', function(){
+    var layer = layui.layer;
+});
+const orderStatusMeta = {
     "insert": 0, // 新增
     "browse": 1, // 浏览
     "delete": 2, // 删除
@@ -11,13 +14,25 @@ var orderStatusMeta = {
     "refresh": 9 // 刷新
 };
 // 默认订单状态为新增
-var defaultOrderStatus = orderStatusMeta.insert;
+const defaultOrderStatus = orderStatusMeta.insert;
 
 let viewModel = new Vue({
     el: "#app",
     data() {
         return {
-            orderStatus: defaultOrderStatus
+            orderStatus: defaultOrderStatus,
+            supplier: "", // 供应商
+            orderDate: "", // 订单日期
+            supplierAddress: "", // 供应商地址
+            orderNumber: "", // 订单号码
+            priceIncludesTax: "", // 单价是否含税
+            currencyType: "", // 货币类型
+            enterWareHouse: "", // 重新入库
+            buyer: "", // 采购人员
+            executor:"", // 制单人员
+            department:"", // 所属部门
+            reviewer:"", // 审核人员
+            project:"" //所属项目
         };
     },
     methods: {
@@ -45,6 +60,33 @@ let viewModel = new Vue({
         },
         unReviewOrder() {
             this.orderStatus = orderStatusMeta.unReview;
+        },
+        showSupplierWindow(){
+            // var targetWindowContent;
+            $.ajax({
+                url: "http://localhost:63342/erp/ERP/window_set_request_purchase_order_type.html",
+                type: "get",
+                success: function (resp) {
+                    // targetWindowContent = resp;
+                    layer.open({
+                        type: 2,
+                        title:"单选--采购请购类型设定",
+                        content: "window_set_request_purchase_order_type.html",
+                        area: ['762px', '450px']
+                    })
+                }/*,
+                async: false*/
+            });
+
+            /*layui.use('layer', function(){
+                var layer = layui.layer;
+                layer.open({
+                    type: 1,
+                    title: '单选--采购请购类型设定',
+                    content: targetWindowContent,
+                    area: ['745px', '450px']
+                });
+            });*/
         }
     },
     watch: {

d55361a897474d9994b96348304368a2e7c8f65a
liuchengpc <1819898784@qq.com>
2020-02-10 10:03:12 +0800

    202002101003lc

--- erp/src/main/java/com/apatech/service/wd_Adjust_priceService.java
+++ erp/src/main/java/com/apatech/service/wd_Adjust_priceService.java
@@ -3,6 +3,8 @@ package com.apatech.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.apatech.domain.Adjust_price;
 import com.apatech.domain.wd_Adjust_price;
@@ -10,14 +12,16 @@ import com.apatech.mapper.Adjust_priceMapper;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
+@Service
+@Transactional
 public class wd_Adjust_priceService {
 	@Autowired
 	private Adjust_priceMapper dao;
 	
-	 public PageInfo<wd_Adjust_price> wdselectAllpage(Integer pageNum,Integer pageSize){
+	 	public PageInfo<wd_Adjust_price> wdselectAllpage(Integer pageNum,Integer pageSize){
     	System.out.println("分页的集合："+dao.wdselectAll().toString());
 		 
-    	PageHelper.startPage(pageNum, pageSize);
+    	PageHelper.startPage(pageNum, pageSize);	
     	List<wd_Adjust_price> list=dao.wdselectAll();
 
     	PageInfo<wd_Adjust_price> page=new PageInfo<wd_Adjust_price>(list);

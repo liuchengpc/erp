e72999ebea479319426032bcf42738b2e1cd405f
HuNao0102 <3078129195@qq.com>
2020-02-23 01:15:18 +0800

    202002230115wd

--- erp/src/main/java/com/apatech/controller/wd_adjustController.java
+++ erp/src/main/java/com/apatech/controller/wd_adjustController.java
@@ -15,8 +15,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apatech.domain.Adjust_price;
+import com.apatech.domain.Sales_out_warehouse;
+import com.apatech.domain.Sales_out_warehouse_detailed;
 import com.apatech.domain.Team;
 import com.apatech.domain.Updown_program;
+import com.apatech.domain.wd_Adjust_detail;
 import com.apatech.domain.wd_Adjust_price;
 import com.apatech.domain.Adjust_price;
 import com.apatech.service.Adjust_priceService;
@@ -42,17 +45,21 @@ public class wd_adjustController {
 	@RequestMapping(value = "selectAllpage",method = RequestMethod.GET)
 	@ResponseBody
 	public PageInfo<wd_Adjust_price> selectAllpage( Integer pageNum,Integer pageSize){
-		//System.out.println("进入分页");
-		//System.out.println(pageNum+"/"+pageSize);
-    	PageInfo<wd_Adjust_price> page=wddao.wdselectAllpage(pageNum, pageSize);
+	
+    	System.out.println(pageNum+"/"+pageSize);
+		PageInfo<wd_Adjust_price> page=wddao.wdselectAllpage(pageNum, pageSize);
+    	for (wd_Adjust_price item : page.getList()) {
+			item.setList(wddao.selectlist(item.getApDateid()));
+		}
+    	
+	    System.out.println("page:"+page.toString());
+	    System.out.println("page.list:"+page.getList().toString());
     	return page;
     }
 	
 	/**
 	 * 查询科目
-	 * @param pageNum
-	 * @param pageSize
-	 * @return
+	 * 
 	 */
 	@RequestMapping(value = "selectkm",method = RequestMethod.GET)
 	@ResponseBody
@@ -103,6 +110,84 @@ public class wd_adjustController {
 	public int selectcount() {
 		return wddao.selectcount();
 	}
+	
+	@RequestMapping("/insert")
+	@ResponseBody
+	public int insert(@RequestBody wd_Adjust_price record) {
+		int apId=selectcount()+1;
+		String apDateid=record.getApDateid();
+		String updownmid=record.getUpdowmid();
+		String updownmids=updownmid+1;
+		String apdoworkman=record.getApDoworkman();
+		String apRecheckman=record.getApRecheckman();
+		String apAuditing="0";
+		String apYn="0";
+		String apCustom6=record.getApCustom6();
+		
+		String upname=record.getUpName();
+		String doname=record.getUpCustom6();
+		System.out.println("新增增减值科目"+updownmids+upname+apDateid+doname);
+		dao.insertupd(updownmids,upname,apDateid,doname);
+		System.out.println("新增主单："+apId+apDateid+updownmids+apdoworkman+apRecheckman+apAuditing+apYn+apCustom6);
+		int i=dao.insert(apId,apDateid,updownmids,apdoworkman,apRecheckman,apAuditing,apYn,apCustom6);
+		if(i>0) {
+			for (wd_Adjust_detail item : record.getList()) {
+				//item.setdApid(record.getApId());
+				String dMatterid=item.getdMatterid();
+				String dAdjustprice=item.getdAdjustprice();
+				String dDecoration=item.getdDecoration();
+				String dApId=item.getdApid();
+				System.out.println("新增详单："+dMatterid+dAdjustprice+dDecoration+dApId);
+				wddao.inserts(dMatterid,dAdjustprice,dDecoration,dApId);
+			}
+			return 1;
+		}else {
+			return 0;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	@RequestMapping("/update")
+	@ResponseBody
+	public int update(@RequestBody wd_Adjust_price record) {
+		String updowmid=record.getUpdowmid();
+		String apDateid=record.getApDateid();
+		//1
+		String upId=record.getUpId();
+		System.out.println("upId"+upId);
+		//空：不知道为啥
+		String upname=record.getUpName();
+		System.out.println("upname"+upname);
+		String doId=record.getUpCustom5();
+		String doname=record.getUpCustom6();
+		System.out.println("upid:"+upId);
+		//修改科目
+		int i=wddao.updatekm(upId,upname,doId,doname,updowmid,apDateid);
+		
+		//修改调价价格
+
+		for (wd_Adjust_detail item : record.getList()) {
+			String dadjustprice=item.getdAdjustprice();
+			String dapid=item.getdApid();
+			String apid=item.getApId();
+			String dMatterid=item.getdMatterid();
+			String dDecoration=item.getdDecoration();
+			//item.setdApid(record.getApDateid());
+			wddao.updateprice(dadjustprice,dDecoration,dMatterid,apid);
+		}
+		//return dao.updateByPrimaryKey(record);
+		return wddao.updatelist(apDateid,updowmid);
+		
+	}	
+	
+	
+
 	
 	
 	@RequestMapping("/updateByPrimaryKeySelective")

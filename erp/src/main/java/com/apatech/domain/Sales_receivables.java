8f73cb7dc00d9dc2e59fd58c06dcb3b69ef01cbf
1508231376@qq.com <1508231376@qq.com>
2020-03-10 12:42:46 +0800

    202003101242lw

--- erp/src/main/java/com/apatech/domain/Sales_receivables.java
+++ erp/src/main/java/com/apatech/domain/Sales_receivables.java
@@ -78,6 +78,20 @@ public class Sales_receivables {
 		this.srCustom9 = srCustom9;
 		this.srCustom10 = srCustom10;
 	}
+    
+    @Override
+	public String toString() {
+		return "Sales_receivables [srId=" + srId + ", srType=" + srType + ", srNumber=" + srNumber + ", srDate="
+				+ srDate + ", srPcId=" + srPcId + ", srPcName=" + srPcName + ", srTeamId=" + srTeamId + ", srTeamName="
+				+ srTeamName + ", srBuyer=" + srBuyer + ", srProjectName=" + srProjectName + ", srCurrency="
+				+ srCurrency + ", srExchangeRate=" + srExchangeRate + ", srPayablesmainPrice=" + srPayablesmainPrice
+				+ ", srCurrentbalance=" + srCurrentbalance + ", srDiscountamount=" + srDiscountamount
+				+ ", srAmountcharged=" + srAmountcharged + ", srOffsetamount=" + srOffsetamount + ", srAuditing="
+				+ srAuditing + ", srYn=" + srYn + ", srCustom1=" + srCustom1 + ", srCustom2=" + srCustom2
+				+ ", srCustom3=" + srCustom3 + ", srCustom4=" + srCustom4 + ", srCustom5=" + srCustom5 + ", srCustom6="
+				+ srCustom6 + ", srCustom7=" + srCustom7 + ", srCustom8=" + srCustom8 + ", srCustom9=" + srCustom9
+				+ ", srCustom10=" + srCustom10 + "]";
+	}
 	
 
     public Integer getSrId() {
